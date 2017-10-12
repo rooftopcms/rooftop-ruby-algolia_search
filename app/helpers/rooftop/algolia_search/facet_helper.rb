79f35e972ec93db7645c242d3be23d31ccebb5a4
Ed Jones <ed@errorstudio.co.uk>
2017-10-12 17:16:44 +0100

    add opts arg to facet helper

--- app/helpers/rooftop/algolia_search/facet_helper.rb
+++ app/helpers/rooftop/algolia_search/facet_helper.rb
@@ -2,7 +2,7 @@ module Rooftop
   module AlgoliaSearch
     module FacetHelper
 
-      def build_facet_query(hash)
+      def build_facet_query(hash,opts = {})
       #   takes a hash like this:
       #   {
       #     foo: ['bar', 'baz'],
@@ -10,15 +10,31 @@ module Rooftop
       #
       #   }
 
-        # returns this
-        # [["foo:bar", "foo:baz"],"qux:boo"]
-        hash.collect do |k,v|
-          if v.is_a?(Array)
-            v.collect {|val| "#{k}:#{val}"}
-          elsif v.is_a?(String) || v.is_a?(Symbol)
-            "#{k}:#{v}"
+        if opts[:or]
+          # returns this
+          # ["foo:bar", "foo:baz", "qux:boo"]
+          result = []
+          hash.each do |k,v|
+            if v.is_a?(Array)
+              v.each {|val| result << "#{k}:#{val}"}
+            else
+              result << "#{k}:#{v}"
+            end
           end
+          return [result]
+        else
+          # returns this
+          # [["foo:bar", "foo:baz"],"qux:boo"]
+          hash.collect do |k,v|
+            if v.is_a?(Array)
+              v.collect {|val| "#{k}:#{val}"}
+            else
+              "#{k}:#{v}"
+            end
+          end
+
         end
+
       end
     end
   end
